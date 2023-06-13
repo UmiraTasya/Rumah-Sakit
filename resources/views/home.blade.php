dc67ac091ef55a6fd24c97d3634f9cec0e2753cb
UmiraTasya <umiratasya80@gmail.com>
2023-06-13 08:00:45 +0700

    message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -152,7 +152,7 @@
             </div>
             <div class="col">
                 <div class="card ml-5" style="width: 18rem;">
-                    <img src="../../assets/img/doctors/4.jpg" class="card-img-top" alt="...">
+                    <img src="../../assets/img/doctors/5.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
